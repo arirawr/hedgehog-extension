717ada018a32f3dd21e8aef21e7b54792c95197d
Arielle VanIderstine <arielle1459@gmail.com>
2015-03-15 00:36:24 -0400

    adding in css and js

---
+++ scripts.js
@@ -0,0 +1,8 @@
+button1 = document.getElementById("button1");
+
+button1.addEventListener("click", onButtonClick);
+
+function onButtonClick() {
+	alert("hey");
+	//document.getElementById('hideme').show();
+}
